b55404e7ce82f39e3d25c27c5bf282bff5272218
Md Jakir Hosen <hiddenclue0@gmail.com>
2025-01-09 15:31:33 +0600

    returning function added

--- javascript/leveling-Function/app.js
+++ javascript/leveling-Function/app.js
@@ -1,14 +1,50 @@
 
 
-function func(){
-    let random = Math.ceil(Math.random() * 6);
-    console.log(random);
-}
+// function func(){
+//     let random = Math.ceil(Math.random() * 6);
+//     console.log(random);
+// }
 
-function callTwice(func){
-    func();
-    func();
+// function callTwice(func){
+//     func();
+//     func();
     
+// }
+
+// callTwice(func);
+
+
+
+
+
+
+function makeMistryFunc(){
+    let random = Math.random();
+
+    if(random > 0.5){
+        return function(){
+            console.log("Congrats, I am a good fucntion!");
+            console.log("You Win a million dollars!");
+        }
+    }
+    else{
+        return function(){
+            alert ("You have infacted by a computer virus");
+            alert("Stop trying to close this window");
+            alert("Stop trying to close this window");
+            alert("Stop trying to close this window");
+            alert("Stop trying to close this window");
+            alert("Stop trying to close this window");
+        }
+    }
 }
 
-callTwice(func);
+function makeBetweenFunc(min, max){
+    return function(num){
+        return num>=min && num <= max;
+    }
+}
+
+const isChild = makeBetweenFunc(0,18);
+const isAdult = makeBetweenFunc(19,64);
+const isSenior = makeBetweenFunc(65, 120);
