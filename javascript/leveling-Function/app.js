753cb2245d878d1a1075b14a7f6ce5a59d2d8486
Md Jakir Hosen <hiddenclue0@gmail.com>
2025-01-09 15:44:52 +0600

    self defined object function

--- javascript/leveling-Function/app.js
+++ javascript/leveling-Function/app.js
@@ -18,33 +18,44 @@
 
 
 
-function makeMistryFunc(){
-    let random = Math.random();
+// function makeMistryFunc(){
+//     let random = Math.random();
 
-    if(random > 0.5){
-        return function(){
-            console.log("Congrats, I am a good fucntion!");
-            console.log("You Win a million dollars!");
-        }
-    }
-    else{
-        return function(){
-            alert ("You have infacted by a computer virus");
-            alert("Stop trying to close this window");
-            alert("Stop trying to close this window");
-            alert("Stop trying to close this window");
-            alert("Stop trying to close this window");
-            alert("Stop trying to close this window");
-        }
-    }
-}
+//     if(random > 0.5){
+//         return function(){
+//             console.log("Congrats, I am a good fucntion!");
+//             console.log("You Win a million dollars!");
+//         }
+//     }
+//     else{
+//         return function(){
+//             alert ("You have infacted by a computer virus");
+//             alert("Stop trying to close this window");
+//             alert("Stop trying to close this window");
+//             alert("Stop trying to close this window");
+//             alert("Stop trying to close this window");
+//             alert("Stop trying to close this window");
+//         }
+//     }
+// }
 
-function makeBetweenFunc(min, max){
-    return function(num){
-        return num>=min && num <= max;
-    }
-}
+// function makeBetweenFunc(min, max){
+//     return function(num){
+//         return num>=min && num <= max;
+//     }
+// }
 
-const isChild = makeBetweenFunc(0,18);
-const isAdult = makeBetweenFunc(19,64);
-const isSenior = makeBetweenFunc(65, 120);
+// const isChild = makeBetweenFunc(0,18);
+// const isAdult = makeBetweenFunc(19,64);
+// const isSenior = makeBetweenFunc(65, 120);
+
+
+const myMath = {
+    PI: 3.12159,
+    square: function(num){
+        return num*num;
+    },
+    cube: function(num){
+        return num ** 3;
+    }
+};
