04412730c4b85937cbea3f8077673a9909862a6d
Md Jakir Hosen <hiddenclue0@gmail.com>
2025-01-09 17:17:22 +0600

    map added

--- javascript/CallBack-Arrray-Method/app.js
+++ javascript/CallBack-Arrray-Method/app.js
@@ -1,8 +1,53 @@
+// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+// numbers.forEach(function (num){
+//     console.log(num);
+// });
+
+
+// const movies = [
+//     {
+//         title: 'Jakir',
+//         score:7
+//     },
+//     {
+//         title: 'Hosen',
+//         score:6
+//     },
+//     {
+//         title: 'Is',
+//         score:5
+//     },
+//     {
+//         title: 'Here',
+//         score:4
+//     },
+//     {
+//         title: 'TOday',
+//         score:3
+//     },
+//     {
+//         title: 'For ',
+//         score:2
+//     },
+//     {
+//         title: 'You',
+//         score:1
+//     }
+// ]
+
+
+// movies.forEach(function(movie){
+//     console.log(`${movie.title} - ${movie.score}/100`)
+// })
+
+
 const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-numbers.forEach(function (num){
-    console.log(num);
-});
+
+const double = numbers.map(function(num){
+    console.log(num*2);
+})
 
 
 const movies = [
@@ -36,7 +81,6 @@ const movies = [
     }
 ]
 
-
-movies.forEach(function(movie){
-    console.log(`${movie.title} - ${movie.score}/100`)
+const title = movies.map(function(movie){
+    console.log(movie.title, movie.score + "/100");
 })
