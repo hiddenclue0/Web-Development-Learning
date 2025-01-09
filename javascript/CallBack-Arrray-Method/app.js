7c52618eeea3488889243facede3878af368f240
Md Jakir Hosen <hiddenclue0@gmail.com>
2025-01-09 17:25:26 +0600

    map practice added

--- javascript/CallBack-Arrray-Method/app.js
+++ javascript/CallBack-Arrray-Method/app.js
@@ -4,6 +4,46 @@
 //     console.log(num);
 // });
 
+// const movies = [
+//     {
+//         title: 'Jakir',
+//         score:7
+//     },
+//     {
+//         title: 'Hosen',
+//         score:6
+//     },
+//     {
+//         title: 'Is',
+//         score:5
+//     },
+//     {
+//         title: 'Here',
+//         score:4
+//     },
+//     {
+//         title: 'TOday',
+//         score:3
+//     },
+//     {
+//         title: 'For ',
+//         score:2
+//     },
+//     {
+//         title: 'You',
+//         score:1
+//     }
+// ]
+
+// movies.forEach(function(movie){
+//     console.log(`${movie.title} - ${movie.score}/100`)
+// })
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+// const double = numbers.map(function(num){
+//     console.log(num*2);
+// })
 
 // const movies = [
 //     {
@@ -36,51 +76,20 @@
 //     }
 // ]
 
-
-// movies.forEach(function(movie){
-//     console.log(`${movie.title} - ${movie.score}/100`)
+// const title = movies.map(function(movie){
+//     console.log(movie.title, movie.score + "/100");
 // })
 
+const fullNames = [
+  { first: "Albus", last: "Dumbledore" },
+  { first: "Harry", last: "Potter" },
+  { first: "Hermione", last: "Granger" },
+  { first: "Ron", last: "Weasley" },
+  { first: "Rubeus", last: "Hagrid" },
+  { first: "Minerva", last: "McGonagall" },
+  { first: "Severus", last: "Snape" },
+];
 
-const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-
-
-const double = numbers.map(function(num){
-    console.log(num*2);
-})
-
-
-const movies = [
-    {
-        title: 'Jakir',
-        score:7
-    },
-    {
-        title: 'Hosen',
-        score:6
-    },
-    {
-        title: 'Is',
-        score:5
-    },
-    {
-        title: 'Here',
-        score:4
-    },
-    {
-        title: 'TOday',
-        score:3
-    },
-    {
-        title: 'For ',
-        score:2
-    },
-    {
-        title: 'You',
-        score:1
-    }
-]
-
-const title = movies.map(function(movie){
-    console.log(movie.title, movie.score + "/100");
+ const firstNames = fullNames.map(function(name){
+    return name.first;
 })
