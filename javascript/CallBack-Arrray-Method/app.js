70c40ae2434dcf49aa76eddbfaed575288c56774
Md Jakir Hosen <hiddenclue0@gmail.com>
2025-01-09 19:10:14 +0600

    this does work on arrow function

--- javascript/CallBack-Arrray-Method/app.js
+++ javascript/CallBack-Arrray-Method/app.js
@@ -189,49 +189,60 @@
 // console.log(even);
 //  const allEvens = arr => arr.every(n => n%2 === 0)
 
-const prices = [9.99, 1.5, 19.99, 49.99, 30.5];
-const totalPrice = prices.reduce((min, price) => {
-  if (min > price) return price;
-  else return min;
-});
+// const prices = [9.99, 1.5, 19.99, 49.99, 30.5];
+// const totalPrice = prices.reduce((min, price) => {
+//   if (min > price) return price;
+//   else return min;
+// });
 
-const movies = [
-  {
-    title: "Jakir",
-    score: 7,
-  },
-  {
-    title: "Hosen",
-    score: 6,
-  },
-  {
-    title: "Is",
-    score: 5,
-  },
-  {
-    title: "Here",
-    score: 4,
-  },
-  {
-    title: "TOday",
-    score: 3,
-  },
-  {
-    title: "For ",
-    score: 2,
-  },
-  {
-    title: "You",
-    score: 1,
-  },
-];
+// const movies = [
+//   {
+//     title: "Jakir",
+//     score: 7,
+//   },
+//   {
+//     title: "Hosen",
+//     score: 6,
+//   },
+//   {
+//     title: "Is",
+//     score: 5,
+//   },
+//   {
+//     title: "Here",
+//     score: 4,
+//   },
+//   {
+//     title: "TOday",
+//     score: 3,
+//   },
+//   {
+//     title: "For ",
+//     score: 2,
+//   },
+//   {
+//     title: "You",
+//     score: 1,
+//   },
+// ];
 
-const best = movies.reduce((bestMovie, currentMovie) => {
-  if (bestMovie.score < currentMovie.score) {
-    return currentMovie;
-  } else {
-    return bestMovie;
-  }
-});
+// const best = movies.reduce((bestMovie, currentMovie) => {
+//   if (bestMovie.score < currentMovie.score) {
+//     return currentMovie;
+//   } else {
+//     return bestMovie;
+//   }
+// });
 
-const evens = arr => arr.reduce((sum, x) => sum+x, 1000);
+// const evens = arr => arr.reduce((sum, x) => sum+x, 1000);
+
+
+
+
+const prerson = {
+    firstName: 'Vigo',
+    lastName: 'Mortensen',
+    fullName: function() {
+        return  `${this.firstName} ${this.lastName}`
+    }
+}
